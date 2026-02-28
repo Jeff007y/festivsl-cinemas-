2f963efa676d8164f0c9ee283b98ef9deb989584
Jeff007y <86217475+Jeff007y@users.noreply.github.com>
2026-02-28 14:28:51 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Vision from './components/Vision';
